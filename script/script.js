aa4479c701463f0b245c1829210db6c1570e1006
Roman Pestryakov <pestr-roman@yandex.ru>
2022-02-12 12:28:53 +0300

    fix: исправлен js

--- script/script.js
+++ script/script.js
@@ -1,37 +1,46 @@
-let buttonOpen = document.querySelector('.profile__edit-button'); // Редактируем профиль
-let popup = document.querySelector('.popup'); // Получаем попап
-let crossClose = document.querySelector('.popup__close-cross');  // Получаем крестик закрытия попапа
-let buttonSave = document.querySelector('.popup__submit'); // Получаем кнопку отправки формы
-let inputName = document.querySelector('.popup__input_name'); // Получаем инпут с именем
-let inputAbout = document.querySelector('.popup__input_about'); // Получаем инпут с информацией о себе
-let profileName = document.querySelector('.profile__name'); // Получаем селектор с именем
-let profileText = document.querySelector('.profile__text'); // Получаем селектор с информацией о себе
+const buttonOpen = document.querySelector('.profile__edit-button'); // Редактируем профиль
+const popup = document.querySelector('.popup'); // Получаем попап
+const crossClose = document.querySelector('.popup__close-cross');  // Получаем крестик закрытия попапа
+const buttonSave = document.querySelector('.popup__submit'); // Получаем кнопку отправки формы
+const inputName = document.querySelector('.popup__input_name'); // Получаем инпут с именем
+const inputAbout = document.querySelector('.popup__input_about'); // Получаем инпут с информацией о себе
+const profileName = document.querySelector('.profile__name'); // Получаем селектор с именем
+const profileText = document.querySelector('.profile__text'); // Получаем селектор с информацией о себе
+const form = document.querySelector('.popup__form'); // Получаем селектор с информацией о себе
 
-// Получаем функциюю клика по открытию попапа
-buttonOpen.addEventListener('click', function (event) {
+// Обработчки клика по открытию попапа
+buttonOpen.addEventListener('click', closeProfilePopup);
+
+// Обработчки клика по закрытию попапа
+crossClose.addEventListener('click', closeProfilePopup);
+
+// Обработчки клика по закрытию попапа в любом месте экрана кроме попапа
+popup.addEventListener('click', closeProfilePopup);
+
+// Получаем функцию по открытию и закрытию попапа
+function closeProfilePopup(event) {
   event.preventDefault();
-  popup.classList.add('popup_opened');
-})
-
-// Получаем функциюю клика по закрытию попапа
-crossClose.addEventListener('click', function (event) {
-  event.preventDefault();
-  popup.classList.remove('popup_opened');
-})
-
-// Получаем функциюю клика по закрытию попапа в любом месте экрана кроме попапа
-popup.addEventListener('click', function (event) {
   if (event.target === popup) {
-    popup.classList.remove('popup_opened');
+    popup.classList.toggle('popup_opened')
   }
-})
+  else if (event.currentTarget === buttonOpen) {
+    inputName.value = profileName.textContent;
+    inputAbout.value = profileText.textContent;
+    popup.classList.toggle('popup_opened')
+  }
+  else if (event.currentTarget === crossClose) {
+    popup.classList.toggle('popup_opened')
+  }
+}
 
-// Получаем функциюю клика отпрвке формы
-buttonSave.addEventListener('click', function (event) {
+// Обработчик клика по отпрвке формы
+form.addEventListener('submit', sendForm);
+buttonSave.addEventListener('click', sendForm);
+
+// Получаем функцию по отпрвке формы
+function sendForm(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileText.textContent = inputAbout.value;
-  inputName.value = '';
-  inputAbout.value = '';
   popup.classList.remove('popup_opened');
-})
+}
