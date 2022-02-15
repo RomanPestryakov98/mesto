a0fc9ba4bfb38013fc386928790f80211a358874
Roman Pestryakov <pestr-roman@yandex.ru>
2022-02-15 19:16:26 +0300

    feat: открытие попапа с картинкой

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
+// Попап профиля
 const buttonOpen = document.querySelector('.profile__edit-button'); // Редактируем профиль
-const popup = document.querySelector('.popup'); // Получаем попап
-const crossClose = document.querySelector('.popup__close-cross');  // Получаем крестик закрытия попапа
-const buttonSave = document.querySelector('.popup__submit'); // Получаем кнопку отправки формы
+const popupProfile = document.querySelector('.popup_type_profile'); // Получаем попап
+const crossCloseProfile = document.querySelector('.popup__close_type_profile');  // Получаем крестик закрытия попапа
 const inputName = document.querySelector('.popup__input_type_name'); // Получаем инпут с именем
 const inputAbout = document.querySelector('.popup__input_type_about'); // Получаем инпут с информацией о себе
 const profileName = document.querySelector('.profile__name'); // Получаем селектор с именем
 const profileText = document.querySelector('.profile__text'); // Получаем селектор с информацией о себе
-const form = document.querySelector('.popup__form'); // Получаем форму
+const formProfile = document.querySelector('.popup__form'); // Получаем форму
 
-
+// Попап добавления карточки
 const buttonAddCard = document.querySelector('.profile__add-button'); // Кнопка по добавлению карточки
 const popupAddCard = document.querySelector('.popup_type_add-card'); // Получаем попап по добавлению карточки
 const inputTitle = document.querySelector('.popup__input_type_title'); // Получаем инпут с заголовком
@@ -16,14 +16,22 @@ const inputLink = document.querySelector('.popup__input_type_link'); // Полу
 const closeAddCard = document.querySelector('.popup__close_type_add-card');  // Получаем крестик закрытия попапа по добавлению карточки
 const formAddCard = document.querySelector('.popup__form_type_add-card'); // Получаем форму по добавлению карточки
 
+// Попап открытой картинки
+const popupOpenImage = document.querySelector('.popup_type_image'); // Получаем попап по открытию картинки
+const imagesCards = document.querySelectorAll('.element__image'); // Получаем картинки из карточек
+const crossCloseOpenImage = document.querySelector('.popup__close_type_open');  // Получаем крестик закрытия попапа по открытию картинки
+
 
 const sectionElements = document.querySelector('.elements'); // Контейнер с карточками
 const cardTemplate = document.querySelector('#add-card').content; // template
 
+
+// Лайки
 const likes = document.querySelectorAll('.element__like'); // Получаем кнопки лайка
 const deleteCards = document.querySelectorAll('.element__delete'); // Получаем кнопки удалить карточку
 
 
+// Массив с карточками
 const initialCards = [
   {
     name: 'Цветные скалы Чжанъе Данксиа',
@@ -72,19 +80,25 @@ buttonOpen.addEventListener('click', openProfilePopup);
 buttonAddCard.addEventListener('click', openAddCardPopup);
 
 // Обработчки клика по закрытию попапа
-crossClose.addEventListener('click', closeProfilePopup);
+crossCloseProfile.addEventListener('click', closeProfilePopup);
 
 // Обработчки клика по закрытию попапа по добавлению карточки
 closeAddCard.addEventListener('click', closeAddCardPopup);
 
+// Обработчки клика по закрытию попапа по добавлению карточки
+crossCloseOpenImage.addEventListener('click', closeOpenImagePopup);
+
 // Обработчки клика по закрытию попапа в любом месте экрана кроме попапа
-popup.addEventListener('click', handleOverlay);
+popupProfile.addEventListener('click', handleOverlay);
 
 // Обработчки клика по закрытию попапа c добавлением карточки в любом месте экрана кроме попапа
 popupAddCard.addEventListener('click', handleOverlayAddCard);
 
+// Обработчки клика по закрытию попапа c добавлением карточки в любом месте экрана кроме попапа
+popupOpenImage.addEventListener('click', handleOverlayOpenImage);
+
 // Обработчик отправки формы
-form.addEventListener('submit', sendForm);
+formProfile.addEventListener('submit', sendForm);
 
 // Обработчик отправки формы по добавлению карточки
 formAddCard.addEventListener('submit', sendFormAddCard);
@@ -99,6 +113,23 @@ for (let i = 0; i < likes.length; i++) {
 for (let i = 0; i < deleteCards.length; i++) {
   let card = deleteCards[i];
   card.addEventListener('click', deleteCard);
+}
+
+// Обработчик клика по картинки
+for (let i = 0; i < imagesCards.length; i++) {
+  let image = imagesCards[i];
+  image.addEventListener('click', openImage);
+}
+
+// Получаем открытие картинки
+function openImage() {
+  const popupImage = document.querySelector('.popup_type_image');
+  let contentImage = popupImage.querySelector('.popup__image');
+  let titleImage = popupImage.querySelector('.popup__label');
+  let title = this.parentElement.parentElement.querySelector('.element__title');
+  contentImage.src = this.src;
+  titleImage.textContent = title.textContent;
+  popupImage.classList.add('popup_opened');
 }
 
 // Получаем функцию добавления лайка
@@ -117,7 +148,7 @@ function deleteCard() {
 function openProfilePopup() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileText.textContent;
-  popup.classList.add('popup_opened');
+  popupProfile.classList.add('popup_opened');
 }
 
 // Получаем функцию по открытию попапа с добавлением карточки
@@ -127,7 +158,7 @@ function openAddCardPopup() {
 
 // Получаем функцию по закрытию попапа
 function closeProfilePopup() {
-  popup.classList.remove('popup_opened');
+  popupProfile.classList.remove('popup_opened');
 }
 
 // Получаем функцию по закрытию попапа с добавлением карточки
@@ -135,9 +166,14 @@ function closeAddCardPopup() {
   popupAddCard.classList.remove('popup_opened');
 }
 
+// Получаем функцию по закрытию попапа c открытием картинки
+function closeOpenImagePopup() {
+  popupOpenImage.classList.remove('popup_opened');
+}
+
 // Получаем функцию по закрытию попапа по клику в любой области кроме самой формы
 function handleOverlay(event) {
-  if (event.target === popup) {
+  if (event.target === popupProfile) {
     closeProfilePopup();
   }
 }
@@ -146,6 +182,13 @@ function handleOverlay(event) {
 function handleOverlayAddCard(event) {
   if (event.target === popupAddCard) {
     closeAddCardPopup();
+  }
+}
+
+// Получаем функцию по закрытию попапа с открытием картинки по клику в любой области кроме самой формы
+function handleOverlayOpenImage(event) {
+  if (event.target === popupOpenImage) {
+    closeOpenImagePopup();
   }
 }
 
@@ -188,5 +231,12 @@ function addCardInContainer() {
   for (let i = 0; i < deleteCards.length; i++) {
     let card = deleteCards[i];
     card.addEventListener('click', deleteCard);
+  }
+
+  const imagesCards = document.querySelectorAll('.element__image'); // Получаем картинки из карточек
+  // Обработчик клика по картинки
+  for (let i = 0; i < imagesCards.length; i++) {
+    let image = imagesCards[i];
+    image.addEventListener('click', openImage);
   }
 }
